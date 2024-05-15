413e573fbf9ce47b39ffdb630fa1fe816006dc40
Jur93n <jurgen.kluft@gmail.com>
2024-05-15 20:32:27 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 git clone --bare https://bitbucket.org/durdn/cfg.git $HOME/.mac-os-dotfiles
 function config {
-   /opt/homebrew/bin/git --git-dir=$HOME/.mac-os-dotfiles/ --work-tree=$HOME $@
+   /opt/homebrew/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
 mkdir -p .config-backup
 config checkout
@@ -9,7 +9,7 @@ if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .mac-os-dotfiles-backup/{}
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
